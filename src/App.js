5a3ab38b4c3de2312876aa6aea25e9eea0ed3323
imad mohamed <abmohammedimad00@gmail.com>
2024-11-29 01:41:46 +0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
 
   return (
-    <div>
-      <h1>app tests</h1>
+    <div className='app'>
+      <h1>app test</h1>
     </div>
   )
 }
