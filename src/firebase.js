ad08f584e5db535ec7a2e4fccfb5a54213afb4cd
imad mohamed <abmohammedimad00@gmail.com>
2024-11-28 22:14:57 +0400

    fireBace Settup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDEciCY6TzETZ51i49poSWsIqJt9Ivs19k",
+    authDomain: "netflix-clone-idart.firebaseapp.com",
+    projectId: "netflix-clone-idart",
+    storageBucket: "netflix-clone-idart.firebasestorage.app",
+    messagingSenderId: "192961858646",
+    appId: "1:192961858646:web:95583ccb6052bbcbc37ff7",
+    measurementId: "G-GH7B3ZDQ1B"
+  };
